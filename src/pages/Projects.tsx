0ee3d89253b67d61d8299db74f08ff2c7a1daefc
shrijo <jonas.shriqui@gmail.com>
2024-05-13 14:51:48 +0200

    nav aligned

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,18 +1,22 @@
 const projects = [
-  { id: "Porjects", span: "span 2", link: "projects" },
-  { id: "Websites", span: "span 1", link: "websites" },
-  { id: "Porjects", span: "span 2", link: "projects" },
-  { id: "Websites", span: "span 1", link: "websites" },
-  { id: "Porjects", span: "span 2", link: "projects" },
-  { id: "Websites", span: "span 1", link: "websites" },
+  { id: "0", name: "asdf", span: "span 2", link: "projects" },
+  { id: "1", name: "ölkj", span: "span 1", link: "websites" },
+  { id: "2", name: "asdf", span: "span 2", link: "projects" },
+  { id: "3", name: "ölkj", span: "span 1", link: "websites" },
+  { id: "4", name: "asdf", span: "span 2", link: "projects" },
+  { id: "5", name: "ölkj", span: "span 1", link: "websites" },
 ];
 
 function Projects() {
   return (
     <div className="grid">
       {projects.map((project) => (
-        <div className="grid-tile" style={{ gridColumn: project.span }}>
-          <h3>{project.id}</h3>
+        <div
+          key={project.id}
+          className="grid-tile"
+          style={{ gridColumn: project.span }}
+        >
+          <h3>{project.name}</h3>
           <div className="grid-tile-image"></div>
         </div>
       ))}
