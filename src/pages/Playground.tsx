3b91c0b9a993b94aae4f610f74df205385c18ff2
shrijo <jonas.shriqui@gmail.com>
2024-05-13 15:06:54 +0200

    add r3f

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,5 +1,27 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 function Playground() {
-  return <h2>Playground</h2>;
+  return (
+    <div className="canvas-container">
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 }
 
 export default Playground;
