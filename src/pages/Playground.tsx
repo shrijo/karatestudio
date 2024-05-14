95bfae13d0c34a367fe54a53f4801d66ab3602cb
shrijo <jonas.shriqui@gmail.com>
2024-05-14 08:05:35 +0200

    removed scene

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,56 +1,16 @@
 import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
 
-function Box(props: any) {
-  // This reference gives us direct access to the THREE.Mesh object
-  // const ref = useRef();
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  // useFrame((state, delta) => (ref.current.rotation.x += delta));
-  // Return the view, these are regular Threejs elements expressed in JSX
+export default function Playground() {
+  const ref = useRef();
   return (
-    <mesh
-      {...props}
-      // ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "darkgray" : "gray"} />
-    </mesh>
-  );
-}
-
-function Playground() {
-  return (
-    <div className="content">
-      <div className="canvas-container">
-        <Canvas>
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight
-            position={[10, 10, 10]}
-            angle={0.15}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <pointLight
-            position={[-10, -10, -10]}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
-          <OrbitControls />
-        </Canvas>
+    <div className="container">
+      <div className="text">
+        "But afterwards there occurred violent earthquakes and floods; and in a
+        single day and night of rain all your warlike men in a body sank into
+        the earth, and the island of Atlantis in like manner disappeared, and
+        was sunk beneath the sea."
       </div>
     </div>
   );
 }
-
-export default Playground;
